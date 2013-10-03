4e8c0b06d2c665058327cf9d49c8ceb71168168e
Matthew Monroe <matthew.monroe@pnnl.gov>
2013-10-03 22:03:12 +0000

    Update the DtaManager to support scan numbers having leading zeroes (required, in particular, for scans less than 1000)

--- AScore_DLL/Managers/DtaManager.cs
+++ AScore_DLL/Managers/DtaManager.cs
@@ -93,10 +93,20 @@ namespace AScore_DLL.Managers
 		//Gets a spectra entry from the dta file
 		public string GetDtaFileName(int scanNumber, int scanCount, int chargeState)
 		{
-			
+			return GetDtaFileName(scanNumber, scanCount, chargeState, scanPrefixPad: "");
+		}
+
+		public string GetDtaFileName(int scanNumber, int scanCount, int chargeState, string scanPrefixPad)
+		{
+			if (string.IsNullOrWhiteSpace(scanPrefixPad))
+				scanPrefixPad = string.Empty;
+
+			string scanStart = scanPrefixPad + scanNumber;
+			string scanEnd = scanPrefixPad + (scanNumber + scanCount - 1);
+
 			return string.Format("{0}.{1}.{2}.{3}.dta",
-				datasetName, scanNumber,
-				scanNumber + scanCount - 1, chargeState);
+				datasetName, scanStart,
+				scanEnd, chargeState);
 		}
 
 
@@ -105,114 +115,133 @@ namespace AScore_DLL.Managers
 		/// <summary>
 		/// Retrieves an experimental spectra entry from the Master DTA file.
 		/// </summary>
-		/// <param name="spectraName">Name of the spectra to retrieve</param>
 		/// <returns>A newly constructed ExperimentalSpectra if the specified
 		/// spectra name exists, null if it does not.</returns>
 		public ExperimentalSpectra GetExperimentalSpectra(int scanNumber, int scanCount, int psmChargeState)
 		{
 			int dtaChargeState = psmChargeState;
-			string spectraName;
-			ExperimentalSpectra expSpec = null;
+			string spectraName = String.Empty;
 
-			spectraName = GetDtaFileName(scanNumber, scanCount, dtaChargeState);
-			if (!dtaEntries.ContainsKey(spectraName))
+			// Find the desired spectrum
+			// Dictionary keys are the header text for each DTA in the _DTA.txt file, for example:
+			// MyDataset.0538.0538.3.dta
+			// Note that scans could have one or more leading zeroes, so we need to account for that
+
+			for (int padLength = 0; padLength <= 6; padLength++)
 			{
+				var scanPrefixPad = new string('0', padLength);
+				spectraName = GetDtaFileName(scanNumber, scanCount, dtaChargeState, scanPrefixPad);
+
+				if (dtaEntries.ContainsKey(spectraName))
+					break;
+								
 				// Try different charge states
+				bool matchFound = false;
 				for (dtaChargeState = 1; dtaChargeState < 6; dtaChargeState++)
 				{
 					spectraName = GetDtaFileName(scanNumber, scanCount, dtaChargeState);
 					if (dtaEntries.ContainsKey(spectraName))
+					{
+						matchFound = true;
 						break;
+					}
 				}
+
+				if (matchFound)
+					break;
 			}
-			
-			if (dtaEntries.ContainsKey(spectraName))
+
+
+			if (!dtaEntries.ContainsKey(spectraName))
+				return null;
+
+			double precursorMass = 0.0;
+			int precursorChargeState = 0;
+			var entries = new List<ExperimentalSpectraEntry>();
+
+			// Set the Master DTAs file position to the specified spectra
+			masterDta.DiscardBufferedData();
+			masterDta.BaseStream.Position = dtaEntries[spectraName];
+
+			// Read the first line of the entry and extract the precursor
+			// entries as well as the scan number and charge state
+			string line = masterDta.ReadLine();
+
+			if (string.IsNullOrWhiteSpace(line))
 			{
-
-				double precursorMass = 0.0;
-				int precursorChargeState = 0;
-				List<ExperimentalSpectraEntry> entries =
-					new List<ExperimentalSpectraEntry>();
-
-				// Set the Master DTAs file position to the specified spectra
-				masterDta.DiscardBufferedData();
-				masterDta.BaseStream.Position = dtaEntries[spectraName];
-
-				// Read the first line of the entry and extract the precursor
-				// entries as well as the scan number and charge state
-				string line = masterDta.ReadLine();
-
-				// Determine the precursor mass
-				// The mass listed in the DTA file is the M+H mass
-				int ind1 = 0;
-				int ind2 = line.IndexOf(' ');
-				double.TryParse(line.Substring(ind1, ind2), out precursorMass);
-
-				// Parse out charge state
-				ind1 = ind2 + 1;
-				ind2 = line.IndexOf(' ', ind1);
-                if (ind2 > -1)
-                {
-                    int.TryParse(line.Substring(ind1, ind2 - ind1), out precursorChargeState);
-
-                    // Parse out scan number (if it's present)
-                    ind1 = line.IndexOf('=') + 1;
-                    ind2 = line.IndexOf(' ', ind1);
-                    int.TryParse(line.Substring(ind1, ind2 - ind1), out scanNumber);
-
-                    // Charge state
-                    ind1 = line.LastIndexOf('=') + 1;
-					int.TryParse(line.Substring(ind1, 1), out dtaChargeState);
-                }
-                else
-                {
-                    int.TryParse(line.Substring(ind1), out precursorChargeState);
-                }
-
-				if (precursorChargeState != dtaChargeState)
-				{
-					ReportWarning("Charge state mismatch: dtaChargeState=" + dtaChargeState + " vs. precursorChargeState=" + precursorChargeState);
-					dtaChargeState = precursorChargeState;
-				}
-
-				double val1 = 0.0;
-				double val2 = 0.0;
-
-				// Process the rest of the entries in this spectra
-				line = masterDta.ReadLine();
-				while ((line.Length > 0) && (!line.Contains("=")) &&
-					(!masterDta.EndOfStream))
-				{
-					// Get the first number
-					ind1 = 0;
-					ind2 = line.IndexOf(' ');
-					double.TryParse(line.Substring(ind1, ind2), out val1);
-
-					// Get the second number
-					ind1 = ind2 + 1;
-					double.TryParse(line.Substring(ind1, line.Length - ind1), out val2);
-
-					// Add this entry to the entries list
-					entries.Add(new ExperimentalSpectraEntry(val1, val2));
-
-					// Read the next line
-					line = masterDta.ReadLine();
-				}
-
-				if (precursorChargeState != psmChargeState)
-				{
-					// Convert precursor mass from M+H to m/z
-					double precursorMZ = PHRPReader.clsPeptideMassCalculator.ConvoluteMass(precursorMass, 1, precursorChargeState);
-
-					// Convert precursor m/z to the correct M+H value
-					precursorMass = PHRPReader.clsPeptideMassCalculator.ConvoluteMass(precursorMZ, psmChargeState, 1);
-					precursorChargeState = psmChargeState;
-				}
-
-				// Finally, create the new ExperimentalSpectra
-				expSpec = new ExperimentalSpectra(scanNumber, psmChargeState,
-					precursorMass, precursorChargeState, entries);
+				ReportWarning("Data not found for DTA " + spectraName);
+				return null;
 			}
+
+			// Determine the precursor mass
+			// The mass listed in the DTA file is the M+H mass
+			int ind1 = 0;
+			int ind2 = line.IndexOf(' ');
+			double.TryParse(line.Substring(ind1, ind2), out precursorMass);
+
+			// Parse out charge state
+			ind1 = ind2 + 1;
+			ind2 = line.IndexOf(' ', ind1);
+            if (ind2 > -1)
+            {
+                int.TryParse(line.Substring(ind1, ind2 - ind1), out precursorChargeState);
+
+                // Parse out scan number (if it's present)
+                ind1 = line.IndexOf('=') + 1;
+                ind2 = line.IndexOf(' ', ind1);
+                int.TryParse(line.Substring(ind1, ind2 - ind1), out scanNumber);
+
+                // Charge state
+                ind1 = line.LastIndexOf('=') + 1;
+				int.TryParse(line.Substring(ind1, 1), out dtaChargeState);
+            }
+            else
+            {
+                int.TryParse(line.Substring(ind1), out precursorChargeState);
+            }
+
+			if (precursorChargeState != dtaChargeState)
+			{
+				ReportWarning("Charge state mismatch: dtaChargeState=" + dtaChargeState + " vs. precursorChargeState=" + precursorChargeState);
+				dtaChargeState = precursorChargeState;
+			}
+
+			// Process the rest of the entries in this spectra
+			line = masterDta.ReadLine();
+			while (!string.IsNullOrWhiteSpace(line) && !line.Contains("=") && (!masterDta.EndOfStream))
+			{
+				// Get the first number
+				ind1 = 0;
+				ind2 = line.IndexOf(' ');
+				double val1 = 0.0;
+				double.TryParse(line.Substring(ind1, ind2), out val1);
+
+				// Get the second number
+				ind1 = ind2 + 1;
+				double val2 = 0.0;
+				double.TryParse(line.Substring(ind1, line.Length - ind1), out val2);
+
+				// Add this entry to the entries list
+				entries.Add(new ExperimentalSpectraEntry(val1, val2));
+
+				// Read the next line
+				line = masterDta.ReadLine();
+			}
+
+			if (precursorChargeState != psmChargeState)
+			{
+				// Convert precursor mass from M+H to m/z
+				double precursorMZ = PHRPReader.clsPeptideMassCalculator.ConvoluteMass(precursorMass, 1, precursorChargeState);
+
+				// Convert precursor m/z to the correct M+H value
+				precursorMass = PHRPReader.clsPeptideMassCalculator.ConvoluteMass(precursorMZ, psmChargeState, 1);
+				precursorChargeState = psmChargeState;
+			}
+
+			// Finally, create the new ExperimentalSpectra
+			var expSpec = new ExperimentalSpectra(scanNumber, psmChargeState,
+				precursorMass, precursorChargeState, entries);
+			
 			return expSpec;
 		}
 
