1e0801093ed20394c82b11e44a036201e427f5c2
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-25 15:27:08 -0700

    Ignore empty rows when caching data in the input file

--- AScore_DLL/Utilities.cs
+++ AScore_DLL/Utilities.cs
@@ -3,109 +3,103 @@
 using System;
 using System.Data;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace AScore_DLL
 {
-	class Utilities
-	{
+    class Utilities
+    {
 
-		/// <summary>
-		/// Generates a datatable with all entries being a string
-		/// </summary>
-		/// <param name="fileName">input file name</param>
-		/// <param name="addDataSetName">whether to add the datasetname as a column</param>
-		/// <returns></returns>
-		public static DataTable TextFileToDataTableAssignTypeString(string fileName, bool addDataSetName)
-		{
-		    var dt = new DataTable();
+        /// <summary>
+        /// Generates a datatable with all entries being a string
+        /// </summary>
+        /// <param name="fileName">input file name</param>
+        /// <param name="addDataSetName">whether to add the datasetname as a column</param>
+        /// <returns></returns>
+        public static DataTable TextFileToDataTableAssignTypeString(string fileName, bool addDataSetName)
+        {
+            var dt = new DataTable();
 
+            using (var sr = new StreamReader(new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                // first line has headers
+                if (!sr.EndOfStream)
+                {
+                    var headerLine = sr.ReadLine();
 
+                    if (headerLine == null)
+                    {
+                        // it's empty, that's an error
+                        throw new ApplicationException("The data provided is not in a valid format (empty header line)");
+                    }
 
-			using (var sr = new StreamReader(fileName))
-			{
-				// first line has headers
-			    string line;
-			    string[] fields;
-			    if ((line = sr.ReadLine()) != null)
-				{
-					fields = line.Split('\t', ',');
-					foreach (var s in fields)
-					{
-						dt.Columns.Add(s);
-						dt.Columns[s].DefaultValue = "";
-					}
+                    var headers = headerLine.Split('\t', ',');
+                    foreach (var s in headers)
+                    {
+                        dt.Columns.Add(s);
+                        dt.Columns[s].DefaultValue = "";
+                    }
+                }
+                else
+                {
+                    // it's empty, that's an error
+                    throw new ApplicationException("The data provided is not in a valid format (empty data file)");
+                }
 
-				}
-				else
-				{
-					// it's empty, that's an error
-					throw new ApplicationException("The data provided is not in a valid format.");
-				}
+                // fill the rest of the table; positional
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (string.IsNullOrWhiteSpace(line))
+                        continue;
 
-				// fill the rest of the table; positional
-				while ((line = sr.ReadLine()) != null)
-				{
-					var row = dt.NewRow();
+                    var row = dt.NewRow();
 
-					fields = line.Split('\t', ',');
-					var i = 0;
-					foreach (var s in fields)
-					{
-						row[i] = s;
-						i++;
-					}
-					dt.Rows.Add(row);
-				}
-			}
-			//if (!dt.Columns.Contains("DatasetName") && addDataSetName)
-			//{
-			//    string dataSetName = Regex.Replace(Path.GetFileName(fileName).Split('.')[0],
-			//"_fht|_fht_MSGF|_fht_MSGF_full|_full|_ReporterIons|_MSGF|_cut", "");
+                    var dataColumns = line.Split('\t', ',');
+                    var i = 0;
+                    foreach (var s in dataColumns)
+                    {
+                        row[i] = s;
+                        i++;
+                    }
+                    dt.Rows.Add(row);
+                }
+            }
 
-			//    dt.Columns.Add("DataSetName", typeof(string));
-			//    foreach (DataRow row in dt.Rows)
-			//    {
-			//        row["DataSetName"] = dataSetName;
-			//    }
-			//}
+            return dt;
 
-			return dt;
+        }
 
+        /// <summary>
+        /// Writes a datatable to text file
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="filePath"></param>
+        public static void WriteDataTableToText(DataTable dt, string filePath)
+        {
+            using (var sw = new StreamWriter(filePath))
+            {
+                var headerLine = dt.Columns[0].ColumnName;
+                for (var i = 1; i < dt.Columns.Count; i++)
+                {
+                    headerLine += "\t" + dt.Columns[i].ColumnName;
+                }
+                sw.WriteLine(headerLine);
 
-		}
-
-		/// <summary>
-		/// Writes a datatable to text file
-		/// </summary>
-		/// <param name="dt"></param>
-		/// <param name="filePath"></param>
-		public static void WriteDataTableToText(DataTable dt, string filePath)
-		{
-			using (var sw = new StreamWriter(filePath))
-			{
-				var s = dt.Columns[0].ColumnName;
-				for (var i = 1; i < dt.Columns.Count; i++)
-				{
-					s += "\t" + dt.Columns[i].ColumnName;
-				}
-				sw.WriteLine(s);
-
-				s = string.Empty;
-				foreach (DataRow row in dt.Rows)
-				{
-					s = "" + row[0];
-					for (var i = 1; i < dt.Columns.Count; i++)
-					{
-						s += "\t" + row[i];
-					}
-					sw.WriteLine(s);
-					s = string.Empty;
-				}
+                foreach (DataRow row in dt.Rows)
+                {
+                    var dataLine = row[0].ToString();
+                    for (var i = 1; i < dt.Columns.Count; i++)
+                    {
+                        dataLine += "\t" + row[i];
+                    }
+                    sw.WriteLine(dataLine);
+                }
 
 
-			}
-		}
+            }
+        }
 
 
-	}
+    }
 }
