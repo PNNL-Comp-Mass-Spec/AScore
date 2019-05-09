9538732e9a8c0fc715779ebb49d8f035528cdb70
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-09 16:47:06 -0700

    Update example AScore usage commands

--- AScore_Console/Data/MSGFPlus_ITRAQ4plex_CDTA/Run_AScore.bat
+++ AScore_Console/Data/MSGFPlus_ITRAQ4plex_CDTA/Run_AScore.bat
@@ -14,7 +14,11 @@ echo.
 echo Procesing with %ExePath%
 echo.
 
-%ExePath% -T:msgfplus -F:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_msgfplus_fht.txt -D:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_dta.txt -P:DynMetOx_DynPhos_stat_4plex_iodo_hcd.xml -L:LogFile.txt -U:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_msgfplus_fht_WithAScore.txt
+rem Parameter file DynMetOx_DynPhos_stat_4plex_iodo_hcd.xml tracks STY Phos and Oxidized M, but not Q Pyro Glu
+%ExePath% -T:msgfplus -F:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_msgfplus_fht.txt -D:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_dta.txt -P:DynMetOx_DynPhos_stat_4plex_iodo_hcd.xml -L:LogFile.txt -U:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_msgfplus_fht_WithAScore.txt -Fasta:..\H_sapiens_M_musculus_Trypsin_NCBI_Excerpt.fasta
+
+rem Parameter file DynMetOx_DynPhos_QPyroGlu_stat_4plex_iodo_hcd.xml also tracks loss of 17 from N-terminal Q
+%ExePath% -T:msgfplus -F:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_msgfplus_fht.txt -D:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_dta.txt -P:DynMetOx_DynPhos_QPyroGlu_stat_4plex_iodo_hcd.xml -L:LogFile.txt -U:CPTAC_CompREF_00_iTRAQ_NiNTA_01b_22Mar12_Lynx_12-02-29_msgfplus_fht_WithAScore.txt -Fasta:..\H_sapiens_M_musculus_Trypsin_NCBI_Excerpt.fasta -O:Results_QPyroGlu
 
 :Done
 
--- AScore_Console/Data/MSGFPlus_ITRAQ4plex_mzML/Run_AScore.bat
+++ AScore_Console/Data/MSGFPlus_ITRAQ4plex_mzML/Run_AScore.bat
@@ -14,7 +14,7 @@ echo.
 echo Procesing with %ExePath%
 echo.
 
-%ExePath% -T:msgfplus -F:Dataset_W_S2_Fr_04_2May17_msgfplus_syn.txt -D:Dataset_W_S2_Fr_04_2May17.mzML -P:AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml -U:Dataset_W_S2_Fr_04_2May17_msgfplus_syn_plus_ascore.txt -L:LogFile.txt
+%ExePath% -T:msgfplus -F:Dataset_W_S2_Fr_04_2May17_msgfplus_syn.txt -D:Dataset_W_S2_Fr_04_2May17.mzML -P:AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml -U:Dataset_W_S2_Fr_04_2May17_msgfplus_syn_plus_ascore.txt -L:LogFile.txt -Fasta:..\H_sapiens_M_musculus_RefSeq_Excerpt.fasta
 
 :Done
 
