a3b56a9ae0825170f862b7eaa55ccf94d89af542
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-23 19:12:14 -0700

    Update the batch file with a Linux example

--- AScore_Console/Data/MSGFPlus_ITRAQ4plex_mzML/Run_AScore.bat
+++ AScore_Console/Data/MSGFPlus_ITRAQ4plex_mzML/Run_AScore.bat
@@ -16,6 +16,9 @@ echo.
 
 %ExePath% -T:msgfplus -F:Dataset_W_S2_Fr_04_2May17_msgfplus_syn.txt -D:Dataset_W_S2_Fr_04_2May17.mzML -P:AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml -U:Dataset_W_S2_Fr_04_2May17_msgfplus_syn_plus_ascore.txt -L:LogFile.txt -Fasta:..\H_sapiens_M_musculus_RefSeq_Excerpt.fasta
 
+rem On Linux, use
+rem mono ../../AScore_Console.exe -T:msgfplus -F:Dataset_W_S2_Fr_04_2May17_msgfplus_syn.txt -D:Dataset_W_S2_Fr_04_2May17.mzML -P:AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml -U:Dataset_W_S2_Fr_04_2May17_msgfplus_syn_plus_ascore.txt -L:LogFile.txt -Fasta:../H_sapiens_M_musculus_RefSeq_Excerpt.fasta
+
 :Done
 
 pause
