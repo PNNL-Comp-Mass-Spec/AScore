e6484d899c42270688d3485e61db4bbba9ddbe2a
alchemistmatt <matthew.monroe@pnnl.gov>
2021-05-05 08:17:26 -0700

    Update the distribution file

--- AScore_Console/bin/Distribute_Program.bat
+++ AScore_Console/bin/Distribute_Program.bat
@@ -5,6 +5,9 @@ xcopy Debug\*.dll C:\DMS_Programs\AScore /D /Y
 
 xcopy Debug\*.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common /D /Y
 
+xcopy Debug\*.exe F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\AScore /D /Y
+xcopy Debug\*.dll F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\AScore /D /Y
+
 xcopy Debug\*.exe \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\AScore /D /Y
 xcopy Debug\*.dll \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\AScore /D /Y
 
